64562c3b9837349cf066a5318a6103683827f6d4
Daniel Bennett and Phil Mass <daniel+phil@>
2019-02-13 11:44:57 -0800

    adjust header and styles

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
 function ErrorPage (){
+  const badStyle = {
+    fontSize: '128px'
+  };
   return(
     <div>
       <h2>You are bad!!!!</h2>
+      <span style={badStyle}>🔥•</span>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,41 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 // import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 function Header(){
+  const appStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  };
+
+  const leftStyle = {
+    display: 'flex',
+    alignItems: 'center',
+  };
+
+  const linkStyle = {
+    color: '#f8f8f8',
+    margin: '0 0.75em'
+  };
+
   return(
     <div>
       <AppBar position="static">
-        <h1>Night Market !</h1>
-        <Link to='/'>Home</Link>
-        <Link to='/products'>Products</Link>
+        <div style={appStyle}>
+          <div style={leftStyle}>
+            <IconButton>
+              <MenuIcon/>
+            </IconButton>
+            <h1>Night Market !</h1>
+          </div>
+          <div>
+            <Link style={linkStyle} to='/'>Home</Link>
+            <Link style={linkStyle} to='/products'>Products</Link>
+          </div>
+        </div>
       </AppBar>
     </div>
   );
