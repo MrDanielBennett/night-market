877da0808732420efa9c27981cf370726c36d1fc
Daniel Bennett and Phil Mass <daniel+phil@>
2019-02-13 16:21:57 -0800

    add second deals

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 function Dog(){
   return(
     <div>
-      <h2>These are our wonderful Dog: </h2>
+      <h2 className='sans'>These are our wonderful Dog: </h2>
       <div className="dogPark">
         <div className="dogRun">
           <p className="deals0">Deals!</p>
+          <p className="deals1">Deals!</p>
           <img className="dog" src='https://data.whicdn.com/images/242549186/original.gif'/>
         </div>
       </div>
